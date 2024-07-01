8c9d86b5de9d161b3bf7f58207b8301cfe935c1c
sahilsnaik <sn220222@gmail.com>
2024-07-01 21:41:08 +0530

    complete update user functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -31,20 +31,20 @@ export default function Header() {
         </Link>
 
         <Link to='/about'>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>
-          About
-        </li>
-        </Link>
-
-        <Link to='/profile'>
-        {currentUser? (
-          <img className='rounded-full h-7 w-7
-          object-cover' src = {currentUser.avatar} alt="profile"/>
-        ) : (
-          <li className='text-slate-700 hover:underline'>
-          Sign in
-        </li>
-        )}
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
+          </Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
         </Link>  
       </ul>
       </div>
